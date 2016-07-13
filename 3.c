1ee29293a2e972196550eac69220ed3073f15374
dasasathyan@github.com <dasasathyan@gmail.com>
2016-07-13 15:10:03 +0530

    final one

--- 3.c
+++ 3.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
 int main () {
-   int saridon=40,restyl=20,sqty,rqty,rate,amt;
+   int saridon=40,restyl=20,dolo650=30,sqty,rqty,dqty,rate,amt;
    do {
       printf("\nenter the quantity for saridon..Available stock of saridon is %d\n",saridon);
       scanf("%d",&sqty);
-      if(sqty>0 ){
       printf("\nenter the rate of each saridon\n");
       scanf("%d",&rate);
       amt=sqty*rate;
       saridon-=sqty;
       printf("\nThe total bill amount is %d\n",amt);
       printf("\nThe available stock of sariidon is %d\n", saridon);
-}
-else{
-  printf("\nYou have entered a wrong quantity...Enter the quantity lesser or equal to the available stock\n");
-}
 }while(saridon!=0);
 for(restyl=20;restyl!=0;restyl-rqty){
   printf("\nenter the quantity for restyl..Available stock of restyl is %d\n",restyl);
@@ -26,5 +21,15 @@ for(restyl=20;restyl!=0;restyl-rqty){
   restyl-=rqty;
   printf("\nThe total bill amount for restyl is %d\n",amt);
   printf("\nThe available stock of restyl is %d\n", restyl);
+}
+while(dolo650<=0){
+  printf("\nenter the quantity for dolo650..Available stock of dolo650 is %d\n",dolo650);
+  scanf("%d",&dqty);
+  printf("\nenter the rate of each restyl\n");
+  scanf("%d",&rate);
+  amt=rqty*rate;
+  dolo650-=dqty;
+  printf("\nThe total bill amount for restyl is %d\n",amt);
+  printf("\nThe available stock of restyl is %d\n", dolo650);
 }
 }
